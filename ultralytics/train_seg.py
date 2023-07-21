6f3b243508ac26c2cf10bc993e1b4209a564f9df
jeyakumar_johnny_jonathan <104885230+jjjonathan14@users.noreply.github.com>
2023-07-21 18:54:22 +0530

    trained

--- ultralytics/train_seg.py
+++ ultralytics/train_seg.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO('yolov8l-seg.yaml')  # build a new model from YAML
-model = YOLO('yolov8l-seg.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8l-seg.yaml').load('yolov8l.pt')  # build from YAML and transfer weights
+# # Load a model
+# model = YOLO('yolov8l-seg.yaml')  # build a new model from YAML
+# model = YOLO('yolov8l-seg.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8l-seg.yaml').load('yolov8l.pt')  # build from YAML and transfer weights
+
+model = YOLO('yolov8l.pt')
 
 # Train the model
 model.train(data='cfg/datasets/coco128-seg.yaml', epochs=300, imgsz=512, batch=8, dropout=0.1,)
