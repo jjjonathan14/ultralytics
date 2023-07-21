3be89a2a75fc4e653fe32013a553db0e8865b689
jeyakumar_johnny_jonathan <104885230+jjjonathan14@users.noreply.github.com>
2023-07-21 17:03:36 +0530

    modified

---
+++ ultralytics/train_seg.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8l-seg.yaml')  # build a new model from YAML
+model = YOLO('yolov8l-seg.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8l-seg.yaml').load('yolov8l.pt')  # build from YAML and transfer weights
+
+# Train the model
+model.train(data='cfg/datasets/coco128-seg.yaml', epochs=300, imgsz=512, batch=8, dropout=0.1,)
